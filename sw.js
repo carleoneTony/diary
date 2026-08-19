1a7fd267c5410fe71e7c11f1b1eeffe4dcf3a4ca
carleoneTony <8t877z8jhh@privaterelay.appleid.com>
2026-08-20 00:18:24 +0300

    sw cache v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='quests-v5';
+const C='quests-v6';
 const ASSETS=['./','./index.html','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
